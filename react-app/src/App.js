75430cb1c16327476696b870aa2e1392412577f3
alex <alex.tran234@gmail.com>
2018-04-14 07:15:02 +1000

    add ABI and contract instantiation

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,17 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
 
+var cryptoZombiesABI = require('./cryptozombies_abi.js').abi;
+
 class App extends Component {
   web3js;
   
   state = {
-    web3js: undefined,
+      web3js: undefined, // web3 API
+      cryptoZombies: undefined, // the contract
   }
+
   checkMetamaskWeb3= () => {
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof window.web3 !== 'undefined') {
       // Use Mist/MetaMask's provider
-      this.setState({web3js: new Web3(window.web3.currentProvider)})
+      var cryptoZombiesAddress="0xc4e157D452FBaA20767cFD051099a4ccb7a9A911"; // on Kovan network
+      var web3js = new Web3(window.web3.currentProvider)
+      var cryptoZombies = new web3js.eth.Contract(cryptoZombiesABI, cryptoZombiesAddress);
+      this.setState ({
+        web3js: web3js,
+        cryptoZombies: cryptoZombies,
+      })
     } else {
       // Handle the case where the user doesn't have web3. Probably 
       // show them a message telling them to install Metamask in 
@@ -30,7 +40,7 @@ class App extends Component {
   }
   
   startApp() {
-    // TODO: 
+    // TODO
   }
   
   handleStartButton = () => {
